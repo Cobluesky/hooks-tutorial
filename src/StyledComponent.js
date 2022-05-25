4c9a80b739231776d4f204f37131e8d78ebf7d3f
Bluesky <37494052+Cobluesky@users.noreply.github.com>
2022-05-25 22:50:54 +0900

    2022/05/25
    react media

--- src/StyledComponent.js
+++ src/StyledComponent.js
@@ -1,11 +1,25 @@
 import styled, { css } from 'styled-components';
 
+const sizes = {
+    desktop: 1024,
+    tablet: 768
+};
+
 const Box = styled.div`
     background: ${props => props.color || 'blue'};
     padding: 1rem;
     display: flex;
+    width: 1024px;
+    margin: 0 auto;
+    @media (max-width: 1024px)
+    {
+        width: 768px;
+    }
+    @media (max-width: 768px)
+    {
+        width: 100%;
+    }
 `;
-
 const Button = styled.button`
     background: white;
     color: black;
